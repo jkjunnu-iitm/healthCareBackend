55167c27943e7359641c27c4bf3f81dc05a7e4ea
Jatinkrishna S Gajula <jatinkjunnu@gmail.com>
2024-10-01 20:55:16 +0530

    Version 1

---
+++ src/main/java/com/healthCareAnalyzer/Health_Care_Backend/HealthCareBackendApplication.java
@@ -0,0 +1,13 @@
+package com.healthCareAnalyzer.Health_Care_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthCareBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthCareBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/healthCareAnalyzer/Health_Care_Backend/HealthCareBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healthCareAnalyzer.Health_Care_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthCareBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
