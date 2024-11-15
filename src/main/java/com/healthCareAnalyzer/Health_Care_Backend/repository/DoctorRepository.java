dfaf2dd84a0816fffd45cc3f2a12469d26de6a22
Jatinkrishna S Gajula <jatinkjunnu@gmail.com>
2024-11-15 20:42:05 +0530

    Version 9 implemented appointment booking endpoint for patients.

--- src/main/java/com/healthCareAnalyzer/Health_Care_Backend/repository/DoctorRepository.java
+++ src/main/java/com/healthCareAnalyzer/Health_Care_Backend/repository/DoctorRepository.java
@@ -1,7 +1,6 @@
 package com.healthCareAnalyzer.Health_Care_Backend.repository;
 
 import com.healthCareAnalyzer.Health_Care_Backend.entity.DoctorEntity;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
